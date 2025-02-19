42a17986482254ff9a22ddfe17f43134849d500f
Mustafa Kerem Çifçi <cifcimustafakerem@gmail.com>
2025-02-19 15:29:20 +0300

    Create input field to collect user input in frontend

---
+++ frontend/src/components/MyForm.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import {Input} from "antd";
+
+const MyForm = () => {
+    return(
+        <div>
+            <Input placeholder="İş"></Input>
+            <Button>Ara</Button>
+        </div>
+    )
+}
+export default MyForm
